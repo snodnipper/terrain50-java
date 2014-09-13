91ad15294efc218c95953a843014757a0c4ac4cb
Oliver Snowden <oliversnowden@snodnipper.co.uk>
2014-09-13 19:41:54 +0100

    mem cache key

--- src/main/java/uk/co/ordnancesurvey/elevation/appengine/MemCache.java
+++ src/main/java/uk/co/ordnancesurvey/elevation/appengine/MemCache.java
@@ -3,5 +3,5 @@ package uk.co.ordnancesurvey.elevation.appengine;
 class MemCache {
 
     protected static final String KEY_CACHE_MANAGER = "cache_manager";
-    protected static final String KEY_ZIP_FILE = "zip_file";
+    protected static final String KEY_ZIP_FILE = "zip_files";
 }
